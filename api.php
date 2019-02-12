64a121591af149d2f4e48fd1899eaac07a409a5b
hrido <hridoy.islam7916@gmail.com>
2019-02-12 14:10:47 +0600

    post,delete and update

--- api.php
+++ api.php
@@ -10,22 +10,25 @@ switch ($request){
         getMethod();
         break;
     case 'POST':
-        echo '{"name":" post...Md Towfiqul Islam"}';
+      //  echo '{"name":" post...Md Towfiqul Islam"}';
+       $data=json_decode(file_get_contents('php://input'),true);
+        postMethod($data);
         break;
     case 'PUT':
-        echo '{"name":" put...Md Towfiqul Islam"}';
-        break;
-    case 'PATCH':
-        echo '{"name":" patch...Md Towfiqul Islam"}';
+       // echo '{"name":" put...Md Towfiqul Islam"}';
+        $data=json_decode(file_get_contents('php://input'),true);
+        putMethod($data);
         break;
     case 'DELETE':
-        echo '{"name":" delete...Md Towfiqul Islam"}';
+       // echo '{"name":" delete...Md Towfiqul Islam"}';
+        $data=json_decode(file_get_contents('php://input'),true);
+        deleteMethod($data);
         break;
     default:
         echo '{"name":" Data Not Found"}';
         break;
 }
-
+//get data
 function getMethod(){
     include "db.php";
 $sql="SELECT * FROM demoapi";
@@ -39,4 +42,47 @@ $sql="SELECT * FROM demoapi";
     }else{
         echo '{"Result":" Data Not Found"}';
     }
+}
+// Insert the data
+function postMethod($data){
+   include "db.php";
+
+    $name=$data["name"];
+    $email=$data["email"];
+    $sql="INSERT INTO demoapi(name,email,created_at) VALUES('$name' ,'$email', NOW())";
+
+    if (mysqli_query($conn , $sql)){
+        echo '{"result":" Data Inserted"}';
+    }else{
+        echo '{"result":" Data not Inserted"}';
+    }
+
+}
+//update the data
+function putMethod($data){
+   include "db.php";
+    $id=$data["id"];
+   $name=$data["name"];
+    $email=$data["email"];
+    $sql="UPDATE demoapi SET name='$name', email='$email', created_at=NOW() WHERE id='$id' ";
+
+    if (mysqli_query($conn , $sql)){
+        echo '{"result":" Update Successfully"}';
+    }else{
+        echo '{"result":"not Updated"}';
+    }
+
+}
+//Delete the data
+function deleteMethod($data){
+    include "db.php";
+    $id=$data["id"];
+    $sql="DELETE FROM demoapi WHERE id='$id'";
+
+    if (mysqli_query($conn , $sql)){
+        echo '{"result":" Delete Successfully"}';
+    }else{
+        echo '{"result":"not Deleted"}';
+    }
+
 }
